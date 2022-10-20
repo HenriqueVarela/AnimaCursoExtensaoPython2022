fc7c4072485b9387782c4a3cec29c6fd926e79c6
HenriqueVarela <106633996+HenriqueVarela@users.noreply.github.com>
2022-10-20 00:52:46 +0000

    aprendi a criar variaveis e exibilas na tela

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 #Meu primeiro Projeto Python!!! ( # = comentario em linha )
-#print() =comando de saida 
+#print() =comando de saida
 
 print("Hello World!")
 
-'''
+# Quando quiser guardar uma String (frase)
+nome = "Henrique Emanoel"
+# Quando eu quiser guardar um número inteiro 
+idade = 20
+# Exibir o nome (que esta dentro da variavel Nome)
+print(nome, idade)
 
+
+
+
+
+'''
 Comentario em bloco 
-
 '''
- 
