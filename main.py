b11f92f018b8939a854c0eec03bd10ec0c0ccace
HenriqueVarela <106633996+HenriqueVarela@users.noreply.github.com>
2022-10-27 00:34:34 +0000

    ensinei condicionais

--- main.py
+++ main.py
@@ -13,5 +13,28 @@ print("Sua idade é {}".format(idade))
 #se eu quiser  mostrar o dobro da idade informada ?
 dobro = idade * 2
 
-print(" o dobro da idade informada é {}". format(dobro))
+print(" o dobro da idade informada é {}". format(dobro)) 
 
+#Estrutura condicional - o famoso "SE" (if)
+#Se a pessoa for maior de idade, mostre "Você é maior de idade, ótimo! Já pode beber ou dirigir"
+if idade >= 18:
+  print("Você é maior de idade, ótimo! Já pode beber ou dirigir")
+else:
+  print("Você é xóven ainda, xóven ainda...")
+
+#E se eu quisessem perguntar o gênero (M = Masculino e F = Feminino)
+#Mostre (...e você também precisa/precisou prestar o serviço militar obrigatório)
+genero = input("informe o gênero M= Masculino, F= Feminino, O= Outros: ")
+print("Você precisa ou precisou prestar o serviço militar obrigatório ")
+if idade >= 18 and genero == "M":
+ print("E você precisa ou precisou prestar o serviço militar obrigatório")
+if idade >= 18 and genero == "F":
+  print("Você não pricisa ou não precisou prestar o serviço militar obrigatório")
+
+
+
+
+
+
+
+print("vai ser executada de qualquer jeito")
