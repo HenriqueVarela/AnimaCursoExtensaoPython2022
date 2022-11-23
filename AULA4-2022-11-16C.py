c378446babd422ab38f6304e212d0151c0c7337b
HenriqueVarela <106633996+HenriqueVarela@users.noreply.github.com>
2022-11-23 23:26:00 +0000

    class pessoas

---
+++ AULA4-2022-11-16C.py
@@ -0,0 +1,15 @@
+#1o. passo: importar a biblioteca sqlite3
+import sqlite3
+
+#Passos 2 e 3 virarão uma função conectar()
+def conectar():
+  #2° Passo: Vamos estabelecer uma #conexão com um banco de dados 
+  conexao = sqlite3.connect("dc_universe.db")
+
+
+
+  #3° passo criar um objeto do tipo cursor
+  cursor = conexao.cursor()
+
+  return conexao, cursor
+
---
+++ pessoas.py
@@ -0,0 +1,13 @@
+class Pessoa:
+  #Atributos
+  id = None
+  nome = None
+
+#metodo contrutor
+def __init__(self, id, nome):
+  self.id = id
+  self.nome = id
+
+
+
+
